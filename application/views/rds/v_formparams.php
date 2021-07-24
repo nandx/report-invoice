1b52254bce261c2c05b28ef27c1f9103e2878d09
dimas9999999 <87854867+dimas9999999@users.noreply.github.com>
2021-07-24 16:08:09 +0700

    Update v_formparams.php
    add css

--- application/views/rds/v_formparams.php
+++ application/views/rds/v_formparams.php
@@ -1,25 +1,106 @@
-<html lang="en">
+<html>
 
 <head>
-     <!-- Required meta tags -->
-     <meta charset="utf-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-     <!-- Bootstrap CSS -->
-     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>ASPURJAB</title>
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap4.min.css" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css" crossorigin="anonymous">
 
-     <title>Edit Detail Invoice</title>
+  <!-- Common Plugins -->
+  <link href="<?php echo base_url('assets/lib/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
+
+  <!-- DataTables -->
+  <link href="<?php echo base_url('assets/lib/datatables/jquery.dataTables.min.css') ?>" rel="stylesheet" type="text/css">
+  <link href="<?php echo base_url('assets/lib/datatables/responsive.bootstrap.min.css') ?>" rel="stylesheet" type="text/css">
+  <link href="<?php echo base_url('assets/lib/datatables/buttons.dataTables.css') ?>" rel="stylesheet" type="text/css">
+
+  <!-- Custom Css-->
+  <link href="<?php echo base_url('assets/css/style.css') ?> " rel="stylesheet">
+
+
+
+  <style>
+      .toggle-none {
+        margin-top: -22px;
+      }
+  </style>
 </head>
 
-<body>
-     <div class="container mt-2">
+<body class="horizontal">
+  <!-- ============================================================== -->
+  <!-- 						Topbar Start 							-->
+  <!-- ============================================================== -->
+  <div class="top-bar light-top-bar">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col">
+          <a class="admin-logo" href="index.html">
+            <h1>
+              <img alt="" src="<?php echo base_url('img/logo-header.png')  ?>" class="toggle-none">
+            </h1>
+          </a>
 
+          <ul class="list-inline top-right-nav">
+
+            <li class="dropdown avtar-dropdown">
+              <a data-toggle="dropdown" href="<?php echo base_url('taspen/lookup?cari=all&year=all'); ?>">
+                Non Aspurjab
+              </a>
+            </li>
+            <li class="dropdown avtar-dropdown">
+              <a data-toggle="dropdown" href="<?php echo base_url('taspen/lookupaspurjab?cari=all&year=all'); ?>">
+                Aspurjab
+              </a>
+            </li>
+            <li class="dropdown avtar-dropdown">
+              <a data-toggle="dropdown" href="<?php echo base_url('taspen/lsparams'); ?>">
+                Parameter
+              </a>
+            </li>
+            <li class="dropdown avtar-dropdown">
+              <lable>&nbsp;&nbsp;&nbsp;&nbsp;</lable>
+            </li>
+            <li class="dropdown avtar-dropdown">
+              <lable>&nbsp;&nbsp;&nbsp;&nbsp;</lable>
+            </li>
+            <li class="dropdown avtar-dropdown">
+              <lable>&nbsp;&nbsp;&nbsp;&nbsp;</lable>
+            </li>
+            <li class="dropdown avtar-dropdown">
+              <lable>&nbsp;&nbsp;&nbsp;&nbsp;</lable>
+            </li>
+            <li class="dropdown avtar-dropdown">
+              <lable>&nbsp;&nbsp;&nbsp;&nbsp;</lable>
+            </li>
+            <li class="dropdown avtar-dropdown">
+              <lable>&nbsp;&nbsp;&nbsp;&nbsp;</lable>
+            </li>
+            <li class="dropdown avtar-dropdown">
+              <lable>&nbsp;&nbsp;&nbsp;&nbsp;</lable>
+            </li>
+            <li class="dropdown avtar-dropdown">
+              <lable>&nbsp;&nbsp;&nbsp;&nbsp;</lable>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- ============================================================== -->
+  <!--                        Topbar End                              -->
+  <!-- ============================================================== -->
+
+  <section class="main-content" style="background-color: #E1E8EB">
+
+<br/><br/>
 
           <div class="header">
                <div class="title">
                     <h3>Edit Detail Invoice</h3>
                </div>
-
           </div>
           <?php foreach ($edparams as $dt) { ?>
 
@@ -56,6 +137,12 @@
                </div>
 
      </div>
+ 
+    <footer class="footer">
+      <span>Copyright &copy; 2020 Taspenlife</span>
+    </footer>
+  </section>
+
 
 </body>
 
