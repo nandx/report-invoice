f15e522e738ee3af3ce368465fa9f506729a8df5
Jerry Lasama <44471003+jerrylasama@users.noreply.github.com>
2021-07-26 16:23:15 +0700

    Update TOTALPREMIUM

--- application/models/RdsTaspen.php
+++ application/models/RdsTaspen.php
@@ -872,7 +872,7 @@ class RdsTaspen extends CI_Model
 					inv.NOINVOICE,
 					ind.NOTAS,
 					ind.NAMA_PESERTA,
-					ind.PREMI,
+					M.TOTALPREMIUM,
 					inv.STATUS,
 					ind.MEMBERNO,
 					IIF(DA.IDINSTANSI != '' AND DA.IDINSTANSI != 'Tidak ada di Antara', DA.IDINSTANSI,
